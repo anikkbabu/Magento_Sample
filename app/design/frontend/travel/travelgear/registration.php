daabdf992a965a8fb314f789b95a28730f2a9d00
anikbabu1 <anikkbabu@gmail.com>
2021-09-23 19:43:59 +0530

    stage 1

---
+++ app/design/frontend/travel/travelgear/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+a
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/travel/travelgear', __DIR__);
